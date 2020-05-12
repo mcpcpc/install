b37f7e44e5bc00bd25078aaa28b2f8a0a516ef1f
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2020-05-12 09:41:42 -0400

    Update and rename .profile to env.sh

--- env.sh
+++ env.sh
@@ -6,10 +6,6 @@ export CXXFLAGS=$CFLAGS
 export MAKEFLAGS="-j$CORES"
 export SAMUFLAGS=$MAKEFLAGS
 
-paste () {
-	curl -sF 'clbin=<'"${1:--}" https://clbin.com;
-}
-
 (read -r _ _ k _ </proc/version;IFS=. read -r s _ </proc/uptime;d=$((s/60/60/24));h=$((s/60/60%24));m=$((s/60%60));[ "$d" = 0 ]||u="$u ${d}d";[ "$h" = 0 ]||u=$u\ ${h}h;[ "$m" = 0 ]||u=$u\ ${m}m;. /etc/os-release;while IFS=: read -r a b;do b=${b%kB};case $a in MemT*)_=$((z+=b));_=$((y=b));; Shmem)_=$((z+=b));;MemF*|Buf*|Cac*|SR*)_=$((z-=b));esac;done </proc/meminfo;_=$((z/=1024));_=$((y/=1024));set -- /var/db/k*/ins*/*/;printf '%b\n' "
 [34;1m    ___      [33m${USER:-$(id -un)}[m@[33;1m$(hostname)[m
 [34;1m   ([37m.. [34m|     [31mos[m:     ${PRETTY_NAME:-Linux}
